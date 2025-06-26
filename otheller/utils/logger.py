8c2f7a13b72b96280b144113ec1401d0e25d79e1
Kirimi <215346793+okirimi@users.noreply.github.com>
2025-06-26 23:45:59 +0900

    feat: logging

---
+++ otheller/utils/logger.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+
+
+def setup_logger(level: str) -> logging.Logger:
+    numeric_level = getattr(logging, level.upper(), None)
+    if not isinstance(numeric_level, int):
+        msg = f"Invalid log level: {level}"
+        raise TypeError(msg)
+
+    file_path = "./logs/otheller.log"
+
+    # Create log folder if not exists
+    Path("./logs").mkdir(exist_ok=True)
+
+    file_handler = logging.FileHandler(file_path)
+
+    logging.basicConfig(
+        level=numeric_level,
+        # [2025-06-26 20:13:26 - <filename>:1024 - DEBUG] <message>
+        format="[%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s] %(message)s",
+        handlers=[
+            file_handler,  # output logs to a log file
+        ],
+    )
+    return logging.getLogger(__name__)
